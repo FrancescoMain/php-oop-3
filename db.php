62f1fcc862b258d34c3f18ede32b1397ca984cd9
francesco <cesaranofrancescomain@gmail.com>
2023-01-26 16:08:46 +0100

    add $employee1

--- db.php
+++ db.php
@@ -2,3 +2,5 @@
 
 $salary1 = new Salary(2000, true, true);
 $person1 = new Person('John', 'Doe', '01-01-2000','New York','CF123456');
+
+$employee1 = new Employee("john", "doe", "1990-01-01", "New York", "123456789",$salary1, "2020-01-01");
