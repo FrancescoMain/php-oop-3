31a187ee4e50f96deca1d20b6babc559aee3c3ae
francesco <cesaranofrancescomain@gmail.com>
2023-01-26 16:19:05 +0100

    echo $manager1

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once 'models/Salary.php';
 require_once 'models/Person.php';
 require_once 'models/Employee.php';
+require_once 'models/Manager.php';
 require_once 'db.php';
 
 echo "<h2>SALARY</h2>";
@@ -12,3 +13,6 @@ echo $person1->getHtml();
 echo "<br><br>";
 echo "<h2>Employee</h2>";
 echo $employee1->getHtml();
+echo "<br><br>";
+echo "<h2>Manager</h2>";
+echo $manager1->getHtml();
